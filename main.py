ed03ccbb34477517f9187670354f666c136ae8ff
superchelc <79621386+superchelc@users.noreply.github.com>
2021-03-02 20:47:29 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 def on_forever():
     basic.pause(6950)
     for index in range(4):
-        maqueen.servo_run(maqueen.Servos.S2, -5)
+        maqueen.servo_run(maqueen.Servos.S1, 70)
         basic.pause(200)
-        maqueen.servo_run(maqueen.Servos.S2, 5)
+        maqueen.servo_run(maqueen.Servos.S1, 50)
         basic.pause(200)
-    maqueen.servo_run(maqueen.Servos.S1, 0)
-    maqueen.servo_run(maqueen.Servos.S2, 0)
     basic.pause(4000)
-    for index2 in range(4):
-        maqueen.servo_run(maqueen.Servos.S2, -5)
+    for index2 in range(2):
+        maqueen.servo_run(maqueen.Servos.S1, 150)
         basic.pause(200)
-        maqueen.servo_run(maqueen.Servos.S2, 5)
+        maqueen.servo_run(maqueen.Servos.S1, 130)
         basic.pause(200)
-    maqueen.servo_run(maqueen.Servos.S2, -90)
-    basic.pause(100)
-    for index3 in range(10):
-        maqueen.servo_run(maqueen.Servos.S2, -5)
+    for index3 in range(3):
+        maqueen.servo_run(maqueen.Servos.S1, 70)
         basic.pause(200)
-        maqueen.servo_run(maqueen.Servos.S2, 5)
+        maqueen.servo_run(maqueen.Servos.S1, 50)
         basic.pause(200)
+    for index4 in range(6):
+        maqueen.servo_run(maqueen.Servos.S1, 150)
+        basic.pause(200)
+        maqueen.servo_run(maqueen.Servos.S1, 130)
+        basic.pause(200)
+    for index5 in range(4):
+        maqueen.servo_run(maqueen.Servos.S1, 70)
+        basic.pause(100)
+        maqueen.servo_run(maqueen.Servos.S1, 50)
+        basic.pause(100)
+    maqueen.servo_run(maqueen.Servos.S1, 50)
+    basic.pause(500)
 basic.forever(on_forever)
 
 def on_forever2():
     basic.pause(2500)
-    for index4 in range(1):
+    for index6 in range(1):
         maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 40)
         basic.pause(200)
         maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 40)
@@ -45,7 +53,7 @@ def on_forever2():
     maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 90)
     basic.pause(400)
     maqueen.motor_stop(maqueen.Motors.ALL)
-    for index5 in range(3):
+    for index7 in range(3):
         maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
         basic.pause(200)
         maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
@@ -59,15 +67,22 @@ def on_forever2():
     maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 110)
     basic.pause(400)
     maqueen.motor_stop(maqueen.Motors.ALL)
-    basic.pause(2000)
-    basic.pause(2000)
+    basic.pause(500)
+    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 30)
+    basic.pause(500)
+    maqueen.motor_stop(maqueen.Motors.ALL)
+    basic.pause(1000)
+    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 30)
+    basic.pause(500)
+    maqueen.motor_stop(maqueen.Motors.ALL)
+    basic.pause(1000)
     maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 80)
     basic.pause(400)
     maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 80)
     basic.pause(400)
     maqueen.motor_stop(maqueen.Motors.ALL)
     basic.pause(100)
-    for index6 in range(2):
+    for index8 in range(2):
         maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 30)
         basic.pause(200)
         maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 30)
@@ -76,7 +91,7 @@ def on_forever2():
 basic.forever(on_forever2)
 
 def on_forever3():
-    music.set_volume(180)
+    music.set_volume(120)
     music.set_tempo(180)
     music.play_tone(392, music.beat(BeatFraction.WHOLE))
     music.rest(music.beat(BeatFraction.WHOLE))
@@ -220,28 +235,32 @@ basic.forever(on_forever3)
 
 def on_forever4():
     basic.pause(6950)
-    for index7 in range(4):
-        maqueen.servo_run(maqueen.Servos.S1, 10)
-        basic.pause(200)
-        maqueen.servo_run(maqueen.Servos.S1, 30)
-        basic.pause(200)
-    maqueen.servo_run(maqueen.Servos.S1, 0)
-    maqueen.servo_run(maqueen.Servos.S2, 0)
-    basic.pause(3900)
-    for index8 in range(4):
-        maqueen.servo_run(maqueen.Servos.S1, -5)
-        basic.pause(200)
-        maqueen.servo_run(maqueen.Servos.S1, 5)
-        basic.pause(200)
-    basic.pause(100)
     for index9 in range(4):
-        maqueen.servo_run(maqueen.Servos.S1, 10)
+        maqueen.servo_run(maqueen.Servos.S2, 70)
         basic.pause(200)
-        maqueen.servo_run(maqueen.Servos.S1, 30)
+        maqueen.servo_run(maqueen.Servos.S2, 50)
         basic.pause(200)
-    for index10 in range(4):
-        maqueen.servo_run(maqueen.Servos.S1, -5)
+    basic.pause(4000)
+    for index10 in range(2):
+        maqueen.servo_run(maqueen.Servos.S2, 150)
+        basic.pause(200)
+        maqueen.servo_run(maqueen.Servos.S2, 130)
+        basic.pause(200)
+    for index11 in range(3):
+        maqueen.servo_run(maqueen.Servos.S2, 150)
+        basic.pause(200)
+        maqueen.servo_run(maqueen.Servos.S2, 130)
+        basic.pause(200)
+    for index12 in range(6):
+        maqueen.servo_run(maqueen.Servos.S2, 70)
+        basic.pause(200)
+        maqueen.servo_run(maqueen.Servos.S2, 50)
+        basic.pause(200)
+    for index13 in range(4):
+        maqueen.servo_run(maqueen.Servos.S2, 70)
         basic.pause(100)
-        maqueen.servo_run(maqueen.Servos.S1, 5)
+        maqueen.servo_run(maqueen.Servos.S2, 50)
         basic.pause(100)
+    maqueen.servo_run(maqueen.Servos.S1, 50)
+    basic.pause(500)
 basic.forever(on_forever4)
